3abe94f36cd6c219f7528d1ce5be623835ae5e4b
Aryan Chauhan <chauhanaryan31801@gmail.com>
2025-08-06 07:04:25 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,9 +1,73 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import deepspeed
 import math
-define positional_embeddings(seq_len, embed_dim):
+
+def positional_embeddings(seq_len, embed_dim):
   position = torch.arange(seq_len).unsqueeze(1)
   div_term  = torch.exp(torch.arange(0, embed_dim, 2)*-(math.log(10000.0)/embed_dim))
   pe = torch.zeros(seq_len, embed_dim)
   pe[:, 0::2] = torch.sin(position * div_term)
   pe[:, 1::2] = torch.cos(position * div_term)
   return pe.unsqueeze(0)
+
+class Multihead_Attention(nn.Module):
+  def __init__(self, num_heads, embed_dim):
+    assert embed_dim % num_heads == 0
+    self.num_heads = num_heads
+    self.embed_dim = embed_dim
+    self.head_dim  = embed_dim/num_heads
+
+    self.q_proj = nn.Linear(embed_dim, embed_dim)
+    self.k_proj = nn.Linear(embed_dim, embed_dim)
+    self.v_proj = nn.Linear(embed_dim, embed_dim)
+
+  def forward(self, x, mask=None):
+    B, T, E = x.size()
+    H, D = self.num_heads, self.head_dim
+
+    q_proj = self.q_proj(x)
+    k_proj = self.k_proj(x)
+    v_proj = self.v_proj(x)
+    
+    q = q_proj.view(B, T, H, D).transpose(1,2)
+    k = k_proj.view(B, T, H, D).transpose(1,2)
+    v = v_proj.view(B, T, H, D).transpose(1,2)
+
+    scores = torch.matmul(q, k.transpose(-2,-1))/math.sqrt(D)
+    atten_scores = F.softmax(scores, dim = -1)
+    atten_scores =  torch.matmul(scores, v)
+    atten_output = atten_scores.transpose(1,2).contiguous().view(B, T, E)
+    return atten_output
+
+class Transformer_Block(nn.Module):
+
+
+
+
+
+if __name__ = '__main__':
+  device = ("cuda" if torch.cuda.is_available() else "cpu")
+  model = gptmodel()
+  criterion = nn.CrossEntropyLoss()
+  model_engine, optimiser, _, _ = deepspeed.intialise(
+    model = model,
+    model_parameters = model.parameters()
+    config = 'ds_config.json')
+
+  epochs = 20
+  total_loss = 0
+  for _ in range(epochs):
+    for (inputs, labels) in enumerate(train_loader):
+      inputs, labels = inputs.to(device), labels.to(device)
+      outputs = model_engine(inputs)
+      loss = critertion(labels, outputs)
+      model_engine.backward()
+      model_engine.step()
+      total_loss+=loss.item()
+  # evaluation loop
+      
+
+
+    
